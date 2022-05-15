3089af18defa958e7441acdbe694ff5e529bf3fd
gullinoagus <agullino@itba.edu.ar>
2022-05-15 01:30:17 -0300

    Primeros pasos

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import sys
+from UI import *
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
 def main():
-    return 0
+
+    app = QtWidgets.QApplication(sys.argv)
+    Ventana = MainWindow()
+    Ventana.show()
+    sys.exit(app.exec())
+
 
 
 if __name__ == "__main__":
