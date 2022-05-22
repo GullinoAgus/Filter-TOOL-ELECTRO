4f009de22f8c15f661c75ad6e3748722b27871bf
gullinoagus <agullino@itba.edu.ar>
2022-05-22 19:40:00 -0300

    ...

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 # noinspection PyUnresolvedReferences
 import sys
+import time
 from Filters import LowPass
 import numpy as np
 from UI import *
-
+from matplotlib.backends.qt_compat import QtWidgets
+from matplotlib.backends.backend_qtagg import (
+    FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.figure import Figure
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
+
     @staticmethod
     def ClickBoton():
         print("matame")
 
-    def Prueba(self):
-        print("Probando");
-
 
 def main():
-    #prueba = LowPass.LowPass(50000)
-    #prueba.plotgain()
-    #time = np.linspace(0, 4 * np.pi, 500)
-    #prueba.plotoutput(time, np.cos(time))
     aplicacion = QtWidgets.QApplication(sys.argv)
     ventana = MainWindow()
     ventana.show()
